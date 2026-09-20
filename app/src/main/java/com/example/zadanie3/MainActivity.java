5fa1efcea91dae9a8d00f16f012508a30a585171
Tomasz Hofman <kani98789@gmail.com>
2026-09-20 20:16:51 +0200

    Podpiecie w javie przez findViewById tekstu oraz buttona 2/3

--- app/src/main/java/com/example/zadanie3/MainActivity.java
+++ app/src/main/java/com/example/zadanie3/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.zadanie3;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.buttonik);
+        TextView textView2 = findViewById(R.id.textView2);
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
